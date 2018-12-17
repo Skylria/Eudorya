6cdc87f605105f39248d50375c8559bb053040ee
Skylria <jullyannalovely@gmail.com>
2018-12-17 20:06:15 -0300

    Adding navbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Eudorya</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style type="text/css">
+		@import url('https://fonts.googleapis.com/css?family=Inconsolata');
+
+		body {
+			margin: 0;
+			padding: 0;
+			/*background-color: black;*/
+			background-repeat: no-repeat;
+			color: white;
+		}
+		.navbar {
+			width: 100%;
+			background-color: black;
+			padding-top: 15px;
+			padding-bottom: 15px;
+		}
+
+		nav {
+			/*background-color: black;*/
+			font-family: 'Inconsolata', monospace;
+			font-weight: bold;
+			font-size: 20px;
+			display: inline;
+			text-transform: uppercase;
+			padding-top: ;
+			padding-bottom: 0;
+		}
+
+		ul {
+			margin: 0;
+		}
+
+		.centered {
+			text-align: center;
+		}
+		.rightened {
+			text-align: right;
+		}
+		ul li {
+			list-style: none;
+			display: inline;
+		}
+		li a {
+			text-decoration: none;
+			color: white;
+			padding: 10px;
+		}
+		li a:hover {
+			text-decoration: line-through;
+		}
+		.col-10{
+			width: 80%;
+		}
+		.col-2 {
+			width: 20%;
+		}
+
+	</style>
+</head>
+<body>
+	<div class="navbar">
+		<nav>
+			<ul class="centered">
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">Sobre o Eudorya</a></li>
+				<li><a href="/me">Conheça a adm</a></li>
+			</ul>
+			<!-- <ul class="rightened col-2">
+				<li><a href="/login">Login</a></li>
+			</ul> -->
+		</nav>
+	</div>
+</body>
+</html>
